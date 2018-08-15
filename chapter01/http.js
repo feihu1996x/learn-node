9f2cd3ab0b59e53d57bdef142f0c51fa71a669d2
feihu1996x <gsx0312@qq.com>
2018-08-15 21:21:42 +0800

    update chapter01/http.js

--- chapter01/http.js
+++ chapter01/http.js
@@ -15,11 +15,26 @@
      回调函数，每当一个新的请求到达Web服务器， 它都会被调用
      */
 
-    // 设置http响应头
-    res.writeHead(200, {"Content-Type": "text/plain"}); 
+    // 规范化 url,去掉查询字符串、可选的反斜杠,并把它变成小写
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 
-    // 写入http正文及关闭连接
-    res.end("Hello, World!\n");  
+    // 实现简单的路由
+    switch (path) {
+        case '':
+            // 设置http响应头
+            res.writeHead(200, {"Content-Type": "text/plain"});             
+            // 写入http正文及关闭连接
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+            break;
+    }
  }).listen(8080, "127.0.0.1");
 
  // 在标准输出stdout上输出提示信息
